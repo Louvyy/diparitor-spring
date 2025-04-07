471fd4e8c64afce38783b19ee37e27cf6d32b7c3
Lionia Romas <johndoe@example.com>
2025-04-07 12:20:29 +0200

    initial commit

---
+++ src/main/java/com/mebae/diparitor/DiparitorApplication.java
@@ -0,0 +1,13 @@
+package com.mebae.diparitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiparitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiparitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mebae/diparitor/DiparitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mebae.diparitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiparitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
