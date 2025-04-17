2e67838667b534e54b3b05ef3003f76c4842ab27
Lionia Romas <johndoe@example.com>
2025-04-17 21:11:15 +0200

    feat: add checkstyle

--- src/main/java/com/mebae/diparitor/DiparitorApplication.java
+++ src/main/java/com/mebae/diparitor/DiparitorApplication.java
@@ -3,11 +3,18 @@ package com.mebae.diparitor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Classe principale de l'application Diparitor.
+ * Cette classe contient la méthode `main` qui lance l'application Spring Boot. Elle est annotée
+ * avec {@link SpringBootApplication}, ce qui signifie qu'elle est responsable de la
+ * configuration de l'application et du démarrage du contexte Spring.
+ *
+ * @author Lionia ROMAS
+ * @version 1.0
+ */
 @SpringBootApplication
 public class DiparitorApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DiparitorApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(DiparitorApplication.class, args);
+  }
 }
